a6254fbd1761850b4fec6d50561dec77907f2773
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-08-28 11:06:20 +0530

    update

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,4 +1,4 @@
-const API_ROOT = 'http://localhost:8000/api/v1';
+const API_ROOT = 'https://scavenger-hunt-api-g.herokuapp.com/api/v1';
 
 export const APIUrls = {
   login : () => `${API_ROOT}/branch/login`,
